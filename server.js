00c4a08ea76cf509ca5b00479e78aedf08132632
Pawan Bansal <bansal.pawan96@gmail.com>
2017-07-10 17:25:50 +0530

    added profile_pic support

--- server.js
+++ server.js
@@ -40,6 +40,7 @@ app.use('/api', apiRoutes);
 // Setting up express to serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
+app.use(express.static(path.join(__dirname, 'uploads')));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
