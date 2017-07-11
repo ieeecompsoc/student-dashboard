42e2eeea8f5a51047683f39c35a72dc0881c55a1
Hiten Sharma <sharmahiten3@gmail.com>
2017-07-11 14:17:02 +0530

    Axios Fix

--- src/components/Login/LogIn.jsx
+++ src/components/Login/LogIn.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import axios from 'axios';
 
@@ -17,7 +17,7 @@ class LogIn extends Component {
     }
 
     authFailure = () => {
-        this.setState({ requestFailed: true, isLoading: false })
+        this.setState({requestFailed: true, isLoading: false})
     }
 
     authSuccess = (token) => {
@@ -25,49 +25,32 @@ class LogIn extends Component {
         const thiss = this;
 
         axios({
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                    'x-access-token': token
-                },
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'x-access-token': token
+            },
                 url: '/api/v1/getUsers',
-                mode: 'cors',
+                mode: 'cors'
             })
-            .then(function(response) {
+            .then(function (response) {
                 console.log(response.data[0]);
-                thiss.setState({
-                    data: response.data[0],
-                    loginStatus: true,
-                })
+                thiss.setState({data: response.data[0], loginStatus: true})
             })
-            .catch(function(error) {
+            .catch(function (error) {
                 console.log("error");
             });
 
-        // fetch("http://localhost:4000/api/v1/getUsers",
-        // {
-        //     method: "GET",
-        //     headers: {
-        //       'Content-Type': 'application/x-www-form-urlencoded',
-        //       'x-access-token': token
-        //     },
-        //     mode: 'cors'
-        // })
-        // .then(function(res){return res.json() })
-        // .then((d) => {
-        //   this.setState({
-        //     data: d,
-        //     loginStatus: true
-        //   });
-        //   console.log(d);
-        // }, () => {
-        //   console.log("Token Expired");
-        // })
+        // fetch("http://localhost:4000/api/v1/getUsers", {     method: "GET",
+        // headers: {       'Content-Type': 'application/x-www-form-urlencoded',
+        // 'x-access-token': token     },     mode: 'cors' }) .then(function(res){return
+        // res.json() }) .then((d) => {   this.setState({     data: d,     loginStatus:
+        // true   });   console.log(d); }, () => {   console.log("Token Expired"); })
     }
 
     authenticate = (e) => {
         e.preventDefault();
-        this.setState({ isLoading: true });
+        this.setState({isLoading: true});
 
         console.log("authenticating")
 
@@ -80,74 +63,50 @@ class LogIn extends Component {
         };
 
         axios({
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                },
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
                 url: '/api/v1/authenticate',
                 mode: 'cors',
                 data: JSON.stringify(payload)
             })
-            .then(function(response) {
+            .then(function (response) {
                 authSuccess(response.data.token);
             })
-            .catch(function(error) {
+            .catch(function (error) {
                 authFailure();
             });
 
-        // fetch("http://localhost:4000/api/v1/authenticate",
-        // {
-        //     method: "POST",
-        //     headers: {
-        //       'Content-Type': 'application/x-www-form-urlencoded'
-        //     },
-        //     mode: 'cors',
-        //     body: JSON.stringify(payload)
-        // })
-        // .then(function(res){return res.json() })
-        // .then(function(data){
-        //   console.log(data.token);
-        //   if(data.token) {
-        //     authSuccess(data.token);
-        //   } else {
-        //     console.log("request failed");
-        //     authFailure();
-        //   }
-        // })
+        // fetch("http://localhost:4000/api/v1/authenticate", {     method: "POST",
+        // headers: {       'Content-Type': 'application/x-www-form-urlencoded'     },
+        //   mode: 'cors',     body: JSON.stringify(payload) })
+        // .then(function(res){return res.json() }) .then(function(data){
+        // console.log(data.token);   if(data.token) {     authSuccess(data.token);   }
+        // else {     console.log("request failed");     authFailure();   } })
 
     }
 
     checkAuthentication = () => {
 
         if (this.state.loginStatus) {
-            return (
-                this.props.children && React.cloneElement(this.props.children, {
-                    data: this.state.data
-                })
-            )
+            return (this.props.children && React.cloneElement(this.props.children, {data: this.state.data}))
         } else {
-            return ( <
-                div > {
-                    this.state.isLoading && < p > Loading... < /p>} {!this.state.isLoading &&
-                    <
-                    form onSubmit = { this.authenticate } >
-                    <
-                    label htmlFor = "enrollment" > Enrollment No. < /label> <
-                    input type = "text"
-                    id = "enrollment"
-                    ref = "enrollment"
-                    name = "enrollment" / > < br / >
-                    <
-                    label htmlFor = "password" > Password < /label> <
-                    input type = "password"
-                    id = "password"
-                    ref = "password"
-                    name = "password" / > < br / >
-                    <
-                    button type = "submit" > Submit < /button> < /
-                    form >
-                } <
-                /div>
+            return (
+                <div>
+                {this.state.isLoading && <p> Loading ... </p>}
+                {
+                    !this.state.isLoading && <form onSubmit = { this.authenticate } >
+                        <label htmlFor = "enrollment" > Enrollment No. </label>
+                        <input type = "text" id = "enrollment" ref = "enrollment" name = "enrollment" />
+                        <br />
+                        <label htmlFor = "password" > Password </label>
+                        <input type = "password" id = "password" ref = "password" name = "password" />
+                        <br />
+                        <button type = "submit"> Submit </button>
+                    </form>
+                }
+                </div>
             )
         }
     }
