0d427963ea4839e2e7ad259de0183a282da0d860
Pawan Bansal <bansal.pawan96@gmail.com>
2017-07-18 00:44:34 +0530

    nothing much

--- app/routes/authenticate.js
+++ app/routes/authenticate.js
@@ -3,37 +3,37 @@ var jwt = require('jsonwebtoken');
 var User = require('../models/user');
 var Student = require('../models/student');
 
-var authenticate = function(req, res){
-  User.findOne({"enrollment": req.body.enrollment}, function(err, user){
-    if(err) return res.status(500).json({"success": false, msg: "Something Crashed"});
-    else{
-      if(!user) return res.status(400).json({"success": false, msg: "Authentication failed"});
-      else{
-        user.comparePassword(req.body.password, function(err, isMatch){
-          if(err) return res.status(400).json({"success": false, msg: "Authentication failed"});
-          else{
-            if(!isMatch) return res.status(400).json({"success": false, msg: "Authentication failed"});
-            Student.findOne({"enrollment": req.body.enrollment}, function(err, student){
-              if(err) return res.status(500).json({"success": false, msg: "Something Crashed"});
-              else{
-                if(!student) return res.status(400).json({"success": false, msg: "Student not found"});
-                else{
-                  var token = jwt.sign(student, process.env.TOKEN_KEY, {
-                    expiresIn: 60*60*24*7
-                  });
-                  return res.status(200).json({
-                    success: true,
-                    msg: "Authenticated. Token valid till 7 days",
-                    token: token
-                  });
-                }
-              }
-            });
-          }
-        })
-      }
-    }
-  });
+var authenticate = function (req, res) {
+    User.findOne({"enrollment": req.body.enrollment}, function (err, user) {
+        if (err) return res.status(500).json({"success": false, msg: "Something Crashed"});
+        else {
+            if (!user) return res.status(400).json({"success": false, msg: "Authentication failed"});
+            else {
+                user.comparePassword(req.body.password, function (err, isMatch) {
+                    if (err) return res.status(400).json({"success": false, msg: "Authentication failed"});
+                    else {
+                        if (!isMatch) return res.status(400).json({"success": false, msg: "Authentication failed"});
+                        Student.findOne({"enrollment": req.body.enrollment}, function (err, student) {
+                            if (err) return res.status(500).json({"success": false, msg: "Something Crashed"});
+                            else {
+                                if (!student) return res.status(400).json({"success": false, msg: "Student not found"});
+                                else {
+                                    var token = jwt.sign(student, process.env.TOKEN_KEY, {
+                                        expiresIn: 60 * 60 * 24 * 7
+                                    });
+                                    return res.status(200).json({
+                                        success: true,
+                                        msg: "Authenticated. Token valid till 7 days",
+                                        token: token
+                                    });
+                                }
+                            }
+                        });
+                    }
+                })
+            }
+        }
+    });
 };
 
 module.exports = authenticate;
